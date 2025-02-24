40eb88c0ab3189c31f43447b87978a7e3365ae6e
Melvin Voetberg <git@melvinvoetberg.com>
2025-02-24 18:11:27 +0100

    Update and bun support

--- modules/home-manager/programs/oh-my-posh/detect-framework.sh
+++ modules/home-manager/programs/oh-my-posh/detect-framework.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-if [[ ! -f "package-lock.json" ]]; then
-  exit 0
-fi
-
 frameworks=("hono" "nest" "next" "nuxt" "expo")
 packages=("hono" "@nestjs/core" "next" "nuxt" "expo")
 icons=("" "" "" "󱄆" "")
 
-dependencies=$(jq -c '.packages // {}' "package-lock.json")
+# Check for bun.lock first
+if [[ -f "bun.lock" ]]; then
+  # Get the list of installed packages from bun
+  dependencies=$(bun pm ls)
 
-for i in "${!frameworks[@]}"; do
-  framework="${frameworks[$i]}"
-  package="node_modules/${packages[$i]}"
-  icon="${icons[$i]}"
-  version=$(echo "$dependencies" | jq -r ".\"$package\".version // empty")
+  for i in "${!frameworks[@]}"; do
+    framework="${frameworks[$i]}"
+    package="${packages[$i]}"
+    icon="${icons[$i]}"
+    
+    # Extract version using grep and sed
+    version=$(echo "$dependencies" | grep -E "^[├└]── $package@" | sed -E 's/.*@([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9.]+)?).*/\1/')
+    
+    if [[ -n $version ]]; then
+      echo "$icon $version"
+      exit 0
+    fi
+  done
+# Then check for package-lock.json
+elif [[ -f "package-lock.json" ]]; then
+  dependencies=$(jq -c '.packages // {}' "package-lock.json")
 
-  if [[ -n $version ]]; then
-    echo "$icon $version"
-    exit 0
-  fi
-done
+  for i in "${!frameworks[@]}"; do
+    framework="${frameworks[$i]}"
+    package="node_modules/${packages[$i]}"
+    icon="${icons[$i]}"
+    version=$(echo "$dependencies" | jq -r ".\"$package\".version // empty")
+
+    if [[ -n $version ]]; then
+      echo "$icon $version"
+      exit 0
+    fi
+  done
+fi
 
 exit 0
